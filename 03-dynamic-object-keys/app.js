8afc14bbba0cab8a61c6e4407c0abe9b298d4038
godwin-nj <amadigodwin7@gmail.com>
2022-02-23 16:07:40 +0100

    03-nuggets

--- 03-dynamic-object-keys/app.js
+++ 03-dynamic-object-keys/app.js
@@ -1,13 +1,49 @@
-const person = ['hello', 'great'];
+const person = ["hello", "great"];
 
 const testPerson = person.map(function (item) {
   console.log(item);
 });
 
 const newPerson = person.map(function (item) {
-  if (item === 'hello') {
+  if (item === "hello") {
     return item.toUpperCase();
   }
   return item;
 });
 console.log(newPerson);
+
+// square bracket notation
+const name = {
+  person: "peter",
+  level: 400,
+};
+console.log(name.person, name.level);
+
+// targetting key in an object
+const houses = {
+  lekki: "Dbanj",
+  country: "Norway",
+};
+console.log(houses["lekki"]);
+console.log(houses["country"]);
+
+const key = "Nigeria";
+const tourist = {
+  [key]: "obodo cantle ranch",
+};
+console.log(tourist);
+
+const state = {
+  name: "",
+  country: "",
+  purchase: false,
+};
+
+function updateState(key, value) {
+  state[key] = value;
+}
+
+updateState("name", "Godwin");
+updateState("country", "Nigeria");
+updateState("purchase", true);
+console.log(state);

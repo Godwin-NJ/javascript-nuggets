1f7d45acf4734e94c43afdd137a403dfb0bc1f6e
godwin-nj <amadigodwin7@gmail.com>
2022-02-24 01:25:03 +0100

    08-filter-find

--- 08-filter-find/app.js
+++ 08-filter-find/app.js
@@ -2,37 +2,55 @@
 // Find - returns single instance (object), returns first match, if no match - undefined
 
 const people = [
-  { name: 'bob', age: 20, position: 'developer' },
-  { name: 'peter', age: 25, position: 'designer' },
-  { name: 'susy', age: 30, position: 'the boss' },
-  { name: 'anna', age: 35, position: 'intern' },
+  { name: "bob", age: 20, position: "developer" },
+  { name: "peter", age: 25, position: "designer" },
+  { name: "susy", age: 30, position: "the boss" },
+  { name: "anna", age: 35, position: "intern" },
 ];
+
+const currentStaff = ["bobby", "stephnie", "Elvis"];
+
 // filter
-const youngPeople = people.filter((person) => {
-  // if (person.age < 30) {
-  //   return person;
-  // }
-  return person.age <= 25;
+const staff = people.filter((person) => person.age > 25);
+console.log(staff);
+
+const current = currentStaff.filter((staff) => {
+  return staff === "bobby";
 });
-console.log(youngPeople);
+console.log(current);
 
-const developers = people.filter((person) => person.position === 'developer');
-console.log(developers);
-// no match
-const seniorDevs = people.filter((item) => item.position === 'senior dev');
-console.log(seniorDevs);
 // find
-const peter = people.find((person) => person.name === 'peter');
-console.log(peter);
-// no match
-const oldPerson = people.find((person) => person.age > 35);
-console.log(oldPerson);
-// multiple matches
-const randomPerson = people.find((person) => person.age < 35);
-console.log(randomPerson);
 
-const anna = people.filter((person) => person.name === 'anna');
-console.log(anna);
+const developer = people.find((person) => person.position === "developer");
+console.log(developer);
 
-console.log(peter.position);
-console.log(anna[0].position);
+// BY SMILGA -> CODING ADDICT
+// // filter
+// const youngPeople = people.filter((person) => {
+//   // if (person.age < 30) {
+//   //   return person;
+//   // }
+//   return person.age <= 25;
+// });
+// console.log(youngPeople);
+
+// const developers = people.filter((person) => person.position === 'developer');
+// console.log(developers);
+// // no match
+// const seniorDevs = people.filter((item) => item.position === 'senior dev');
+// console.log(seniorDevs);
+// // find
+// const peter = people.find((person) => person.name === 'peter');
+// console.log(peter);
+// // no match
+// const oldPerson = people.find((person) => person.age > 35);
+// console.log(oldPerson);
+// // multiple matches
+// const randomPerson = people.find((person) => person.age < 35);
+// console.log(randomPerson);
+
+// const anna = people.filter((person) => person.name === 'anna');
+// console.log(anna);
+
+// console.log(peter.position);
+// console.log(anna[0].position);

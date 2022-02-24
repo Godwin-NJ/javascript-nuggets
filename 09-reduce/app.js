5b74d63604f4c897c79093bb9521c031d023637f
godwin-nj <amadigodwin7@gmail.com>
2022-02-24 03:00:45 +0100

    09 nuggets

--- 09-reduce/app.js
+++ 09-reduce/app.js
@@ -5,19 +5,28 @@
 // 2nd parameter ('curr') - current iteration/value
 
 const staff = [
-  { name: 'bob', age: 20, position: 'developer', salary: 100 },
-  { name: 'peter', age: 25, position: 'designer', salary: 300 },
-  { name: 'susy', age: 30, position: 'the boss', salary: 400 },
-  { name: 'anna', age: 35, position: 'intern', salary: 10 },
+  { name: "bob", age: 20, position: "developer", salary: 100 },
+  { name: "peter", age: 25, position: "designer", salary: 300 },
+  { name: "susy", age: 30, position: "the boss", salary: 400 },
+  { name: "anna", age: 35, position: "intern", salary: 10 },
 ];
 
-const dailyTotal = staff.reduce((total, person) => {
-  // console.log(total);
-  // console.log(person.salary);
-  console.log(`Total ${total}`);
-  console.log(`Current ${person.salary}`);
-  total += person.salary;
-  return total;
+const summation = staff.reduce((acc, cur) => {
+  console.log(cur.salary);
+  // console.log(acc);
+  acc += cur.salary;
+  return acc;
 }, 0);
+console.log(summation);
 
-console.log(dailyTotal);
+// john smilga
+// const dailyTotal = staff.reduce((total, person) => {
+//   // console.log(total);
+//   // console.log(person.salary);
+//   console.log(`Total ${total}`);
+//   console.log(`Current ${person.salary}`);
+//   total += person.salary;
+//   return total;
+// }, 0);
+
+// console.log(dailyTotal);
